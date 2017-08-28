c65df73607e5df432fcbc8c38f0755b710458a1a
Jawwad Firdousi <jawwadaziz@>
2017-08-28 11:24:40 +0500

    [minor] remove unused tests

--- spec/file-spec.js
+++ spec/file-spec.js
@@ -2,40 +2,14 @@ const file = require('../lib/file');
 
 /* eslint-disable no-undef */
 
-describe('Get stream for file', () => {
+describe('File Readable stream', () => {
   describe('Get URL stream', () => {
     it('shoud be a request stream', () => {
       const stream = file.getFileStream('https://www.google.com', 'url');
 
       expect(stream.method).toBe('GET');
     });
-    // it('should be a readable file stream', () => {
-    //   const stream = file.getFileStream('../package.json', 'local');
-    //   // console.log(stream);
-    //
-    //   stream.on('readable', (data) => {
-    //     console.log('data-----------------');
-    //     expect(data).toBe('abc');
-    //   });
-    //   stream.on('end', (err) => {
-    //     console.log('data-----------------');
-    //     expect(data).toBe('abc');
-    //
-    //     console.log(err);
-    //   });
-    //   stream.on('close', (err) => {
-    //     console.log('data-----------------');
-    //     expect(data).toBe('abc');
-    //
-    //     console.log(err);
-    //   });
-    //   stream.on('data', (err) => {
-    //     console.log('data-----------------');
-    //     expect(data).toBe('abc');
-    //
-    //     console.log(err);
-    //   });
-    // });
+
     it('should throw undefined type', () => {
       expect(
         function () {
